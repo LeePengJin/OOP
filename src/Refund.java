6cf3a5bd99aee10ff3a2ac1da236e45c83aa4766
qitzern1127 <130295829+qitzern1127@users.noreply.github.com>
2023-09-14 00:15:23 +0800

    Update Refund.java

--- src/Refund.java
+++ src/Refund.java
@@ -1,41 +1,136 @@
+import java.util.Date;
+
 public class Refund {
-
+    // Data members
     private String refundID;
-    private String refundDate;
-    private String refundStatus;
+    private String paymentID; // Assuming this identifies the order associated with the refund
+    private double refundAmount;
+    private Date refundDate;
 
-    public Refund(){
-        this("", "", "");
+    // Constructors
+    public Refund() {
+        this("", "", 0.0, new Date());
     }
 
-    public Refund(String refundID, String refundDate, String refundStatus){
+    public Refund(String refundID, String paymentID, double refundAmount, Date refundDate) {
         this.refundID = refundID;
+        this.paymentID = paymentID;
+        this.refundAmount = refundAmount;
         this.refundDate = refundDate;
-        this.refundStatus = refundStatus;
     }
 
+    // Getters
     public String getRefundID() {
         return refundID;
     }
-
-    public String getRefundDate() {
+    public String getPaymentID() {
+        return paymentID;
+    }
+    public double getRefundAmount() {
+        return refundAmount;
+    }
+    public Date getRefundDate() {
         return refundDate;
     }
 
-    public String getRefundStatus() {
-        return refundStatus;
-    }
-
+    // Setters
     public void setRefundID(String refundID) {
         this.refundID = refundID;
     }
-
-    public void setRefundDate(String refundDate) {
+    public void setPaymentID(String paymentID) {
+        this.paymentID = paymentID;
+    }
+    public void setRefundAmount(double refundAmount) {
+        this.refundAmount = refundAmount;
+    }
+    public void setRefundDate(Date refundDate) {
         this.refundDate = refundDate;
     }
+    public void recordRefundDetails() {
+        try {
+            String driverClassName = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/oop";
+            String username = "root";
+            String password = "";
 
-    public void setRefundStatus(String refundStatus) {
-        this.refundStatus = refundStatus;
+            // Load driver class
+            Class.forName(driverClassName);
+
+            // Obtain a connection
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            // Prepare SQL statement to insert refund details into a refunds table
+            String insertQuery = "INSERT INTO refunds (refundID, paymentID, refundAmount, refundDate) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = con.prepareStatement(insertQuery);
+
+            // Set the parameter values for the SQL statement
+            statement.setString(1, refundID);
+            statement.setString(2, paymentID);
+            statement.setDouble(3, refundAmount);
+            statement.setDate(4, new java.sql.Date(refundDate.getTime()));
+
+            // Execute the SQL statement to insert refund details
+            statement.executeUpdate();
+
+            // Close the connections
+            statement.close();
+            con.close();
+
+            System.out.println("Refund details recorded successfully.");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("An error occurred while recording refund details.");
+        }
     }
 
+    private static boolean isPaymentRecordedInRefunds(String paymentID) {
+        boolean isRecorded = false;
+
+        try {
+            String driverClassName = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/oop";
+            String username = "root";
+            String password = "";
+
+            // Load driver class
+            Class.forName(driverClassName);
+
+            // Obtain a connection
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            // Prepare SQL statement to check if paymentID exists in refunds table
+            String selectQuery = "SELECT COUNT(*) AS count FROM Refund WHERE paymentID = ?";
+            PreparedStatement statement = con.prepareStatement(selectQuery);
+
+            // Set the parameter values for the SQL statement
+            statement.setString(1, paymentID);
+
+            // Execute the SQL statement and get the result
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                int count = resultSet.getInt("count");
+                if (count > 0) {
+                    isRecorded = true;
+                }
+            }
+
+            // Close the connections
+            resultSet.close();
+            statement.close();
+            con.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("An error occurred while checking if payment is recorded.");
+        }
+
+        return isRecorded;
+    }
+    public void displayRefundItem(){
+        Toy[] toysSelected = new Toy[0];
+        double[] newToyPrice = new double[0];
+        int[] toyQuantity = new int[0];
+        int index = 0;
+        Toy.passToPayment(toysSelected, newToyPrice,toyQuantity,index);
+    }
 }
